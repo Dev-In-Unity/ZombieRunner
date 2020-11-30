4214b489b368b82b191609d363984bfb75af7d03
Wesley_Johnson <johnsonwesley167@gmail.com>
2020-11-30 10:10:15 +0530

    019 - Creating DeathHandler.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,11 +8,12 @@ public class SceneLoader : MonoBehaviour
     public void ReloadGame()
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-
+     
 }
